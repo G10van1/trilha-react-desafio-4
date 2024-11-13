2ff2e4103f5e903cd43bb31974178dd07172b8ce
Giovani Nardi <gionardi@hotmail.com>
2024-11-13 16:58:54 -0300

    Habilitar/Desabilitar botão Entrar

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,8 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, onClick, disabled = true }: IButtonProps) => {
+  return <ButtonContainer onClick={onClick} disabled={disabled}>{title}</ButtonContainer>;
 };
 
 export default Button;
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 
+export const ButtonContainer = styled.button<{ disabled?: boolean }>`
+  width: 100%;
+  height: 42px;
+  background-color: #81259D;
+  color: #FFF;
+  border: 1px solid #81259D;
+  border-radius: 21px;
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
 
-export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
-
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  &:hover {
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 0.6)};
+  }
+`;
